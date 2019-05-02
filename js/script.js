1632ee07b1d6780dceda64d4f2b9bcb050e019d0
Lina <venclovaite.lina@gmail.com>
2019-05-02 23:30:38 +0300

    2sekcija, js ir pic pabaigt

--- js/script.js
+++ js/script.js
@@ -6,3 +6,21 @@ function menuIcon() {
     x.className = "nav";
   }
 }
+
+function openContent(evt, content) {
+	let i, tabcontent, tablinks;
+	//surenkam visus tab-content i array
+	tabcontent = document.getElementsByClassName("tab-content");
+	//ir  visiem uzdedam display: none.
+	for (i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";}
+	//uzdedam paklikintam tab-content .active klase
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+	evt.currentTarget.className += " active";
+	//parenkam tab-content kurio id="content" ir padarom jo display: block;
+	document.getElementById(content).style.display = "flex";
+	//document.getElementById("defaultOpen").click();
+}
